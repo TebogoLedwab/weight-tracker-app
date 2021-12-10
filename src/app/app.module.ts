dc5b2e085cb558939adcbb974812683e17508b2d
TebogoLedwab <tebogoledwaba17@gmail.com>
2021-12-10 21:13:44 +0200

    created a new git repository

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
